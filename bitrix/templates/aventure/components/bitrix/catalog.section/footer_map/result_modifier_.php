f21665eb8fbd87c22ed7cba8c122dce639edeef8
dm84 <dmitriy.o84@ya.ru>
2015-04-07 09:37:22 +0500

    footer

---
+++ bitrix/templates/aventure/components/bitrix/catalog.section/footer_map/result_modifier_.php
@@ -0,0 +1,13 @@
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) { die(); }
+global $APPLICATION;
+
+$cp = $this->__component; // объект компонента
+
+if (is_object($cp))
+{
+    // добавим в arResult компонента два поля - MY_TITLE и IS_OBJECT
+    $cp->SetResultCacheKeys(array('NAME','PREVIEW_TEXT', 'DETAIL_TEXT'));
+	
+	$arResult = $cp->arResult;
+}
+?>
--- bitrix/templates/aventure/components/bitrix/catalog.section/footer_map/template.php
+++ bitrix/templates/aventure/components/bitrix/catalog.section/footer_map/template.php
@@ -1,19 +1,2 @@
 <?php
-	
-	include('prolog.php');
-		
-	foreach ($arResult['ITEMS'] as $idx => $item)
-	{
-		$this->AddEditAction($item['ID'], $item['EDIT_LINK'], 
-				CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_EDIT"));
-
-		$this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], 
-				CIBlock::GetArrayByID($arParams["IBLOCK_ID"], "ELEMENT_DELETE"), 
-				array("CONFIRM" => GetMessage('CT_BCS_ELEMENT_DELETE_CONFIRM')));
-
-		$editAreaId = $this->GetEditAreaId($item['ID']);
-		
-		include('item.php');
-	}
-	
-	include('epilog.php');
+	$component->SetResultCacheKeys(array('NAME','PREVIEW_TEXT', 'DETAIL_TEXT'));
